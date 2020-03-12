5466d894547d6ca62a7bd35688bced0d50c7dc2b
wbbrisbane <59631742+wbbrisbane@users.noreply.github.com>
2020-03-11 23:49:47 -0400

    Add files via upload
    Version 2 of generator. Now allows for all potentials to be generated, but only Harmonic oscillator and Inf. potential well have defined energy functions. No root finding method implemented as of yet.

--- data_generation/Gen_main.py
+++ data_generation/Gen_main.py
@@ -2,7 +2,7 @@ import argparse
 from Generator import Generator
 from potentails import *
 
-#VERSON NOTE: only HMO with predefined energy function will work
+#VERSON NOTE: all potential types wil work, no energy function (or root finding) for GPW and DBW
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,17 +21,17 @@ if __name__ == "__main__":
                       size=args.res)
     elif args.pot_type.upper() == "IPW":
         g = Generator(square_pot, \
-                      [(0.0,0.16),(0.0,0.16),(-8.0,8.0),(-8.0,8.0)], \
+                      [(-8.0,8.0),(-8.0,8.0),(4.0,15.0),(0.0,0.4),(0.0,1.0)], \
                       energy=square_nrg if args.use_nrg else None, \
                       size=args.res)
     elif args.pot_type.upper() == "GPW":
         g = Generator(grad_pot, \
-                      [(0.0,0.16),(0.0,0.16),(-8.0,8.0),(-8.0,8.0)], \
+                      [(-8.0,8.0),(-8.0,8.0),(4.0,15.0),(0.0,0.4),(0.0,1.0),(-3.0,3.0),(-3.0,3.0)], \
                       energy=grad_nrg if args.use_nrg else None, \
                       size=args.res)
     elif args.pot_type.upper() == "DPW":
         g = Generator(double_pot, \
-                      [(0.0,0.16),(0.0,0.16),(-8.0,8.0),(-8.0,8.0)], \
+                      [(-8.0,8.0),(-8.0,8.0),(4.0,15.0),(0.0,0.4),(0.0,1.0),(0.05,0.2),(2.0,18.0)], \
                       energy=double_nrg if args.use_nrg else None, \
                       size=args.res)
     else:
@@ -40,9 +40,9 @@ if __name__ == "__main__":
     if g != None:
         for i in range(int(args.n_samps/args.n_per)):
             g.gen_samps(args.n_per)
-            g.export_samps("samples_"+str(args.res)+"_["+str(args.n_per*(i+1))+" of "+str(args.n_samps)+"]")
+            g.export_samps("samples_"+str(args.pot_type)+"_["+str(args.n_per*(i+1))+" of "+str(args.n_samps)+"]")
             g.clear_samps()
         if args.n_samps%args.n_per != 0:
             g.gen_samps(args.n_samps%args.n_per)
-            g.export_samps("samples_"+str(args.res)+"_["+str(args.n_samps)+" of "+str(args.n_samps)+"]")
+            g.export_samps("samples_"+str(args.pot_type)+"_["+str(args.n_samps)+" of "+str(args.n_samps)+"]")
             g.clear_samps()
