eb5dcdd91c3ce66433a2f3187e0d7213eccbb5f2
Meetinder Lail <meetinderlail@yahoo.ca>
2020-03-24 18:16:12 -0400

    Added cuda support and gitignore

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as func
 import numpy as np
 
+
 class Net(nn.Module):
 
-    # INPUTS: 
+    # INPUTS:
     # [...]_kernel = kernel size for the convolution layers
     # [...]_stride = stride for the convolution layers
     # [...]_out = # of output channels for reducing vs. non-reducing layers
@@ -14,17 +15,20 @@ class Net(nn.Module):
         super(Net, self).__init__()
 
         # convolution layers: '[...]_red' is a reducing layer; '[...]_non' is a non-reducing layer
-        self.conv1_red = nn.Conv2d(1, red_out, kernel_size= red_kernel, stride= red_stride) 
-        self.conv2_red = nn.Conv2d(nonred_out, red_out, kernel_size= red_kernel, stride= red_stride)
+        self.conv1_red = nn.Conv2d(
+            1, red_out, kernel_size=red_kernel, stride=red_stride)
+        self.conv2_red = nn.Conv2d(
+            nonred_out, red_out, kernel_size=red_kernel, stride=red_stride)
 
-        self.conv_non = nn.Conv2d(red_out, nonred_out, kernel_size= nonred_kernel, stride= nonred_stride)
+        self.conv_non = nn.Conv2d(
+            red_out, nonred_out, kernel_size=nonred_kernel, stride=nonred_stride)
 
         # size variables based on number of layers
-        fSize= 4    # final size based on number of reducing layers and stride
+        fSize = 4    # final size based on number of reducing layers and stride
 
         # fully connected layers
-        self.fc1=nn.Linear(red_out*fSize*fSize, 1024)
-        self.fc2=nn.Linear(1024, 1)
+        self.fc1 = nn.Linear(red_out*fSize*fSize, 1024)
+        self.fc2 = nn.Linear(1024, 1)
 
     # Feedforward function
     def forward(self, x):
@@ -36,9 +40,10 @@ class Net(nn.Module):
         r3 = func.relu(self.conv2_red(n2))
         n3 = func.relu(self.conv_non(r3))
         r4 = func.relu(self.conv2_red(n3))
-        
+
         # fully connected layers
-        y = r4.view(-1, r4.size(1)*r4.size(2)*r4.size(3))  # flattening before applying linear functions
+        # flattening before applying linear functions
+        y = r4.view(-1, r4.size(1)*r4.size(2)*r4.size(3))
         y = func.relu(self.fc1(y))
         y = func.relu(self.fc2(y))
 
@@ -54,35 +59,35 @@ class Net(nn.Module):
     def backprop(self, image, energy, loss, optimizer):
         self.train()
         # preparing input and target tensors with proper dtype
-        inputs= torch.tensor(image, dtype= torch.double)
-        targets= torch.tensor(energy, dtype= torch.double)
-   
+        inputs = image
+        targets = energy
+
         # calculating output from nn and reshaping
         calculatedVal = self.forward(inputs)
         size = calculatedVal.size()[0]
         calculatedVal = calculatedVal.view(size)
 
         # calculating loss
-        obj_val= loss(calculatedVal, targets)
-        
+        obj_val = loss(calculatedVal, targets)
+
         optimizer.zero_grad()
         obj_val.backward()
         optimizer.step()
-   
+
         return calculatedVal, obj_val.item()
 
     # Test function. Avoids calculation of gradients.
     def test(self, image, energy, loss):
         self.eval()
         with torch.no_grad():
-            inputs= torch.tensor(image, dtype= torch.double)
-            targets= torch.tensor(energy, dtype= torch.double)
+            inputs = image
+            targets = energy
 
             # calculating output from nn and reshaping
             calculatedVal = self.forward(inputs)
             size = calculatedVal.size()[0]
             calculatedVal = calculatedVal.view(size)
 
-            cross_val= loss(calculatedVal, targets)
+            cross_val = loss(calculatedVal, targets)
 
         return cross_val.item()
--- cnn/main.py
+++ cnn/main.py
@@ -9,7 +9,9 @@ Rubin Hazarika (20607919)
 
 import sys
 import numpy as np
-import json, argparse, torch
+import json
+import argparse
+import torch
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -23,18 +25,28 @@ vb = 0
 
 dirName = "plot/"
 
+
+def convertToTensor(tensorObj, tensorType, cudaToggle):
+    if cudaToggle:
+        return torch.tensor(tensorObj, dtype=tensorType).to('cuda')
+    return torch.tensor(tensorObj, dtype=tensorType)
+
+
 if __name__ == '__main__':
-    
+
     # get image file name from cmd line and load in pickled file
     imFile = sys.argv[1]
-    imData = np.load(imFile, allow_pickle=True)   
-    if vb == 1: print("Name of file: ", imFile, " Size of sample file: ", np.shape(imData))
-   
+    enableCuda = int(sys.argv[2])
+    imData = np.load(imFile, allow_pickle=True)
+    if vb == 1:
+        print("Name of file: ", imFile,
+              " Size of sample file: ", np.shape(imData))
+
     # hyperparameters (json file) import
     jsonPath = "parameters.json"
     with open(jsonPath) as json_file:
         param = json.load(json_file)
-    
+
     # reshaping data and identifying labels
     imLen = int(np.sqrt(len(imData[0]) - 1))     # pixel length of square image
     images = []
@@ -42,9 +54,9 @@ if __name__ == '__main__':
 
     # storing energies and images in arrays
     for img in imData:
-       images.append(np.reshape(img[:-1], (1,imLen,imLen)))
-       energies.append(img[-1])
-    
+        images.append(np.reshape(img[:-1], (1, imLen, imLen)))
+        energies.append(img[-1])
+
     # hyperparameters (start with r = for reducing layer, start with n = for nonreducing layer)
     lr_rate = float(param["learning_rate"])
     rKern = int(param["reducing_conv_kernel"])
@@ -55,59 +67,69 @@ if __name__ == '__main__':
     nOut = int(param["nonreducing_out"])
 
     # constructing a model (converting model to double precision)
-    model = Net(red_kernel=rKern, nonred_kernel=nKern, red_stride=rStride, nonred_stride=nStride, red_out=rOut, nonred_out=nOut)
+    model = Net(red_kernel=rKern, nonred_kernel=nKern, red_stride=rStride,
+                nonred_stride=nStride, red_out=rOut, nonred_out=nOut)
     model = model.double()
+
+    if enableCuda:
+        model.to('cuda')
 
     # declare optimizer and gradient and loss function
     optimizer = optim.Adadelta(model.parameters(), lr=lr_rate)
-    loss = torch.nn.MSELoss(reduction= 'mean')
-    
+    loss = torch.nn.MSELoss(reduction='mean')
+
     # storing the training and test loss values
-    obj_vals= []
-    cross_vals= []
-    
+    obj_vals = []
+    cross_vals = []
+
     # epoch hyperparameters
-    epochs = param["num_epochs"] 
+    epochs = param["num_epochs"]
     disp_epochs = param["display_epochs"]
-    num_epochs= int(epochs)
+    num_epochs = int(epochs)
 
     # Sectioning off training and testing images
     cutoff = 140
 
-    images1 = images[:cutoff]
-    test = images[cutoff:]
+    images1 = convertToTensor(images[:cutoff], torch.double, enableCuda)
+    test = convertToTensor(images[cutoff:], torch.double, enableCuda)
 
-    energies1= energies[:cutoff] 
-    testE = energies[cutoff:]
+    energies1 = convertToTensor(energies[:cutoff], torch.double, enableCuda)
+    testE = convertToTensor(energies[cutoff:],  torch.double, enableCuda)
 
     # Training loop
     for epoch in range(1, num_epochs + 1):
-        output, train_val= model.backprop(images1, energies1, loss, optimizer)        # training loss value
-        obj_vals.append(train_val)                                                  # appending loss values for training dataset     
+        output, train_val = model.backprop(
+            images1, energies1, loss, optimizer)        # training loss value
+        # appending loss values for training dataset
+        obj_vals.append(train_val)
 
-        test_val= model.test(test, testE, loss)  
+        test_val = model.test(test, testE, loss)
         cross_vals.append(test_val)
 
         if not ((epoch + 1) % disp_epochs):
-            plt.plot(output.detach().numpy())
-            plt.savefig(dirName + "E" + str(epoch) + ".png")    
+            if enableCuda:
+                output = output.cpu().detach().numpy()
+            else:
+                output = output.detach().numpy()
+
+            plt.plot(output)
+            plt.savefig(dirName + "E" + str(epoch) + ".png")
             plt.close()
 
-            print('Epoch [{}/{}]'.format(epoch, num_epochs)+\
-              '\tTraining Loss: {:.4f}'.format(train_val) + \
-               '\t Test Loss: {:.4f}'.format(test_val))
+            print('Epoch [{}/{}]'.format(epoch, num_epochs) +
+                  '\tTraining Loss: {:.4f}'.format(train_val) +
+                  '\t Test Loss: {:.4f}'.format(test_val))
 
     plt.close()
 
     plt.plot(energies)
-    plt.savefig(dirName + "InitialE.png") 
+    plt.savefig(dirName + "InitialE.png")
     plt.close()
 
     plt.plot(obj_vals)
-    plt.savefig(dirName + "Training error.png") 
+    plt.savefig(dirName + "Training error.png")
     plt.close()
 
     plt.plot(cross_vals)
-    plt.savefig(dirName + "Test error.png") 
+    plt.savefig(dirName + "Test error.png")
     plt.close()
- 
